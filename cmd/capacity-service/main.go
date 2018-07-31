5b517a084ee0883261e3679e6b0bb40df15bc94f
Serhii Aheienko <serhii.aheienko@gmail.com>
2018-07-31 14:36:07 +0300

    Add a capacity-service cmd

---
+++ cmd/capacity-service/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/alexflint/go-arg"
+	"github.com/kubernetes-sigs/kubebuilder/pkg/signals"
+	"github.com/supergiant/capacity/pkg/capacityserver"
+	"github.com/supergiant/capacity/pkg/log"
+	"os"
+)
+
+type args struct {
+	KubescalerConfig string `arg:"--kubescaler-config" env:"CAPACITY_KUBESCALER_CONFIG" help:"path to a kubescaler config"`
+	KubeConfig       string `arg:"--kube-config"       env:"CAPACITY_KUBE_CONFIG"       help:"path to a kubeconfig file"`
+	ListenAddr       string `arg:"--listen-addr"       env:"CAPACITY_LISTEN_ADDR"       help:"address to listen on, pass as a addr:port"`
+	LogLevel         string `arg:"--verbosity"         env:"CAPACITY_LOG_LEVEL"         help:"logging verbosity"`
+}
+
+func (args) Version() string {
+	return "raw"
+}
+
+func main() {
+	// setup the default logger
+	log.New()
+	log.SetOutput(os.Stderr)
+
+	args := &args{
+		KubescalerConfig: "/etc/kubescaler.conf",
+		ListenAddr:       ":8081",
+		LogLevel:         "info",
+	}
+	arg.MustParse(args)
+
+	srv, err := capacityserver.New(capacityserver.Config{
+		KubescalerConfig: args.KubescalerConfig,
+		KubeConfig:       args.KubeConfig,
+		ListenAddr:       args.ListenAddr,
+	})
+	if err != nil {
+		log.Fatalf("capacityserver: %v\n", err)
+	}
+
+	stopCh := signals.SetupSignalHandler()
+	if err = srv.Start(stopCh); err != nil {
+		log.Fatalf("capacityserver: start: %v\n", err)
+	}
+}
---
+++ pkg/capacityserver/server.go
@@ -0,0 +1,53 @@
+package capacityserver
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/supergiant/capacity/pkg/capacityserver/handlers"
+	"github.com/supergiant/capacity/pkg/kubescaler"
+	"github.com/supergiant/capacity/pkg/log"
+)
+
+type Config struct {
+	KubescalerConfig string
+	KubeConfig       string
+	ListenAddr       string
+}
+
+type API struct {
+	ks  *capacity.Kubescaler
+	srv http.Server
+}
+
+func New(conf Config) (*API, error) {
+	ks, err := capacity.New(conf.KubeConfig, conf.KubescalerConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	handler, err := handlers.New(ks)
+	if err != nil {
+		return nil, err
+	}
+
+	return &API{
+		ks: ks,
+		srv: http.Server{
+			Addr:         conf.ListenAddr,
+			Handler:      handler,
+			ReadTimeout:  30 * time.Second,
+			WriteTimeout: 30 * time.Second,
+			IdleTimeout:  120 * time.Second,
+		},
+	}, nil
+}
+
+func (a *API) Start(stopCh <-chan struct{}) error {
+	log.Infof("capacityservice: listen on %q", a.srv.Addr)
+	return a.srv.ListenAndServe()
+}
+
+func (a *API) Shutdown() error {
+	return nil
+}
