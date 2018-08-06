15882085596d69ae78caf3106db1d6ec1ed6802b
Serhii Aheienko <serhii.aheienko@gmail.com>
2018-08-06 14:47:40 +0300

    Add a middleware to set content-type

--- pkg/capacityserver/handlers/handler.go
+++ pkg/capacityserver/handlers/handler.go
@@ -4,23 +4,36 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 
 	"github.com/supergiant/capacity/pkg/capacityserver/handlers/v1"
 	"github.com/supergiant/capacity/pkg/capacityserver/handlers/version"
 	"github.com/supergiant/capacity/pkg/kubescaler"
 )
 
-func Router(ks *capacity.Kubescaler) (*mux.Router, error) {
+func Handler(ks *capacity.Kubescaler) (http.Handler, error) {
 	handlerV1, err := v1.New(ks)
 	if err != nil {
 		return nil, err
 	}
 
 	r := mux.NewRouter()
+	r.Use(
+		mux.MiddlewareFunc(cors.Default().Handler),
+		mux.MiddlewareFunc(setContentType),
+	)
+
 	r.Path("/version").Methods(http.MethodGet).HandlerFunc(version.Handler)
 
 	apiv1 := r.PathPrefix("/api/v1").Subrouter()
 	handlerV1.RegisterTo(apiv1)
 
 	return r, nil
+}
+
+func setContentType(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
